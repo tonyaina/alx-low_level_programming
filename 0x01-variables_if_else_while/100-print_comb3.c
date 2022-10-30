8c50500b1e28bdfcc44babba96689e2b552a8030
tonyaina <t.awolowo19911@gmail.com>
2022-10-30 16:59:28 -0700

    Task 10

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -12,15 +12,15 @@ int main(void)
 	int ones = '0';
 	int tens = '0';
 
-	for (tens = '0'; tens <= '0'; ten++)/* prints tens digit*/
+	for (tens = '0'; tens <= '0'; ten++)
 	{
-		for (ones = '0'; ones <= '0'; ones++)/* prints ones digit*/
+		for (ones = '0'; ones <= '0'; ones++)
 		{
-			if (!((ones == tens) || (tens > ones)))/*eliminates repitition*/
+			if (!((ones == tens) || (tens > ones)))
 			{
 				putchar(tens);
 				putchar(ones);
-				if (!(ones == '9' && tens == '8'))/*addes comma and space*/
+				if (!(ones == '9' && tens == '8'))
 				{
 					putchar(',');
 					putchar(' ');
