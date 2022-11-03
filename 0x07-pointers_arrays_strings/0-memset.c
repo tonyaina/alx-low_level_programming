0e9460d24890342bf2a026000eb6311ed13cf287
tonyaina <t.awolowo19911@gmail.com>
2022-11-03 00:41:28 -0700

    0. memset task

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,28 +1,21 @@
-/*
- * File: 0-memset.c
- * Auth: not
- */
-
 #include "main.h"
 
 /**
- * _memset - fills the first n bytes of the memory area
- * pointer to by @s with the constant byte @c.
- * @s: A pointer to the memory area to be filled.
- * @c: The character to fill the memory area with.
- * @n: the number of bytes to be filled.
- * description  _memset: over there
+ * _memset - fill a block of memory with a specific value
+ * @s: starting address of memory to be filled
+ * @b: the desired value
+ * @n: number of bytes to be changed
  *
- * Return: a pointer to the filleed memory area @s.
- *
+ * Return: changed  array with new value for n bytes
  */
-void *_memset(void *s, int c, size_t n)
+char *_memset(char *s, char b, unsigned int n)
 {
-	unsigned int index;
-	unsigned char *memory = s, value = c;
+	int i = 0;
 
-	for (index = 0; index < n; index++)
-		memory[index] = value;
-
-	return (memory);
+	for (; n > 0; i++)
+	{
+		s[i] = b;
+		n--;
+	}
+	return (s);
 }
