4a749d37ff7115d961c08734bdae8f1922f0856b
tonyaina <t.awolowo19911@gmail.com>
2022-11-10 03:13:46 -0800

    recursion task 0

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -7,13 +7,11 @@
  */
 void _puts_recursion(char *s)
 {
-	if (*s != '\0')
+	if (*s)
 	{
 		_putchar(*s);
 		_puts_recursion(s + 1);
 	}
-	else
-	{
-		_putchar('\n');
-	}
+	if (!=s)
+	_putchar('\n');
 }
