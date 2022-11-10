5e4417774048bc26b33fc82491e080437e0b8a73
tonyaina <t.awolowo19911@gmail.com>
2022-11-10 01:17:58 -0800

    Task 0 recursion

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -2,17 +2,28 @@
 
 /**
  * _puts_recursion - function like puts
+ *
  * @s: input string to print
+ *
  * Return: Always 0 (Success)
  */
+
 void _puts_recursion(char *s)
 {
-	if (*s == '\0')
-	{
-		_putchar('\n');
-		return;
-	}
+	char result = sum(10);
 
-	_putchar(*s);
-	_puts_recursion(s + 1);
+	printf("%d", result'\n');
+	return (0);
+}
+
+void _puts_recursion(char *s)
+{
+	if (*s > 0)
+	{
+		return (*s + sum(*s - 1));
+	}
+	else
+	{
+		return (0);
+	}
 }
