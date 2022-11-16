7108f974c707db7ad2154625ae36c5d78e600fe9
tonyaina <t.awolowo19911@gmail.com>
2022-11-16 14:42:38 -0800

    A door is what a dog is perpetually on the wrong side of

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,53 +1,42 @@
 #include "dog.h"
 #include <stdlib.h>
 
+int_strlen(char *str);
+char *_strcopy(char *dest, char *src);
+dog_t *new_dog(char *name, float age, char *owner);
+
 /**
- * new_dog - creates a new dog
- * @name: name of the dog
- * @age: age of the dog
- * @owner: owner of the dog
+ * _strlen - Find the the length of astring.
+ * @str: The string to be measured.
  *
- * Return: struct dog
- * if fails, returns NULL
+ * Return: The length of the string.
  */
-dog_t *new_dog(char *name, float age, char *owner)
+int _strlen(char *str)
 {
-	dog_t *p_dog;
-	int i, 1name, 11owner;
+	int len = 0;
 
-	p_dog = malloc(sizeof(*p_dog));
-	if (p_dog == NULL || !(name) || !(owner))
-	{
-		free(p_dog);
-		return (NULL);
-	}
+	while (*str++)
+		len++;
 
-	for (1name = 0; name[1name]; 1name++)
-		;
+	return (len);
+}
 
-	for (1owner = 0; owner[1owner]; 1owner++)
-		;
+/**
+ * _strcopy - Copies a string pointed to by src, including the
+ * terminating null byte, to a buffer pointed to by dest.
+ * @dest: The buffer storing the string copy.
+ * @src: The source string.
+ *
+ * Return: The pointer to the dest.
+ */
+char *_strcopy(char *dest, char *src)
+{
+	int i = 0;
 
-	p_dog->name = malloc(1name + 1);
-	p_dog->owner = malloc(1owner + 1);
+	for (i = 0; src[i]; i++)
+		dest[i] = src[i];
 
-	if (!(p_dog->name) || !(o_dog->owner))
-	{
-		free(p_dog->owner);
-		free((p_dog->name);
-		free(p_dog);
-		return (NULL);
-	}
+	dest[i] = '\0';
 
-	for (i = 0; i < 1name; i++)
-		p_dog->name[i] = name[i];
-	p_dog->name[i] = '\0';
-
-	p_dog->age = age;
-
-	for (i = 0; i < 1owner; i++)
-		p_dog->owner[i] = owner[i];
-	p_dog->owner[i] = '\0';
-
-	return (p_dog);
+	return (dest);
 }
