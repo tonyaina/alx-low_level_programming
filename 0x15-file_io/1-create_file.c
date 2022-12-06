a6013a17d66d80068b942fa9c2a332b9d24db024
tonyaina <t.awolowo19911@gmail.com>
2022-12-06 00:55:35 -0800

    1. Under the snow task

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -24,8 +24,7 @@ int create_file(const char *filename, char *text_content)
 	if (!text_content)
 		text_content = "";
 
-	for (nletters = 0; text_content[nletters]; nletters++)
-		;
+	for (nletters = 0; text_content[nletters]; nletters++);
 
 	rwr = write(fd, text_content, nletters);
 
