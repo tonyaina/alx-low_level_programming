cc7ebb25779463d29facb34f3e970057099fdd8c
tonyaina <t.awolowo19911@gmail.com>
2022-12-06 00:34:24 -0800

    0. Tread lightly, she is near tasks

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -5,7 +5,7 @@
  * @filename: filename
  * @letters: numbers of letters printed
  *
- * Return: numbers of letters printed, it fails then return 0
+ * Return: numbers of letters printed, it fails then return 0.
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
@@ -19,7 +19,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	fd = open(filename, O_RDONLY);
 
 	if (fd == -1)
-		retutn (0);
+		return (0);
 
 	buf = malloc(sizeof(char) * (letters));
 	if (!buf)
